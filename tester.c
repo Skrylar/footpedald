5e76f39ec07aeb2887a8dcf285c94c36101562c8
Joshua Cearley <joshua.cearley@gmail.com>
2016-05-18 21:07:47 -0500

    Plumbing to watch for keys and print key status.

--- tester.c
+++ tester.c
@@ -2,13 +2,64 @@
 
 #define DEVICEPATH "/dev/usb/hiddev1"
 
+/* size of individual datablocks */
 #define DATABLOCK_SIZE 18
 typedef char datablock[DATABLOCK_SIZE];
 
-int main() {
-  FILE* dev_input;
-  datablock block;
+/* offsets to bytes we are interested in */
+#define OFF_LEFT 6
+#define OFF_CENTER 14
+#define OFF_RIGHT 22
 
+#define BUTTON_LEFT (1 << 0)
+#define BUTTON_CENTER (1 << 1)
+#define BUTTON_RIGHT (1 << 2)
+
+int mask_from_datablock(datablock* dblock) {
+  int ret = 0;
+  if (dblock[OFF_LEFT] != 0) {
+    ret |= BUTTON_LEFT;
+  }
+  if (dblock[OFF_CENTER] != 0) {
+    ret |= BUTTON_CENTER;
+  }
+  if (dblock[OFF_RIGHT] != 0) {
+    ret |= BUTTON_RIGHT;
+  }
+  return ret;
+}
+
+void print_button_banner(int buttons) {
+  fputs("Left ", stdout);
+  if ((buttons & BUTTON_LEFT) != 0) {
+    fputs("ON\t", stdout);
+  } else {
+    fputs("OFF\t", stdout);
+  }
+
+  fputs("Center ", stdout);
+  if ((buttons & BUTTON_CENTER) != 0) {
+    fputs("ON\t", stdout);
+  } else {
+    fputs("OFF\t", stdout);
+  }
+
+  fputs("Right ", stdout);
+  if ((buttons & BUTTON_RIGHT) != 0) {
+    fputs("ON\t", stdout);
+  } else {
+    fputs("OFF\t", stdout);
+  }
+
+  fputs("\n", stdout);
+}
+
+int main() {
+  FILE* dev_input;		/* fd to read events from */
+  datablock dblock;		/* block to store packets */
+  int button_states = 0;	/* current button states */
+
+  /* header */
   printf("This is footpedald's proof of concept program.\n");
   printf("Not recommended for production use.\n");
 
@@ -20,8 +71,8 @@ int main() {
 
   /* read user events */
   while (ferror(dev_input) == 0) {
-    if (fread((void*)&block, DATABLOCK_SIZE, 1, dev_input)) {
-      fputs("User event.\n", stdout);
+    if (fread((void*)&dblock, DATABLOCK_SIZE, 1, dev_input)) {
+      print_button_banner(mask_from_datablock(&dblock));
     } else {
       fputs("Mutilated user event.\n", stderr);
     }
