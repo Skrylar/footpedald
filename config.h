7766a8c58357cb0f21f2de60c97d4faba1edb1fa
Joshua Cearley <joshua.cearley@gmail.com>
2016-05-26 16:58:19 -0500

    Put footpedald under the GPLv2.

--- config.h
+++ config.h
@@ -1,3 +1,19 @@
+/* This file is part of footpedald.
+
+    footpedald is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    footpedald is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with footpedald.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _25398a7e_a57e_4090_ab72_ee38da949f68_
 #define _25398a7e_a57e_4090_ab72_ee38da949f68_
 #pragma once
